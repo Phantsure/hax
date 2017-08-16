faa22e0a469e570c73f7427961a06949d649a1b1
Divyansh Kamboj <agilephoenix815.dk@gmail.com>
2017-08-16 17:51:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,94 @@
 import os, slackclient, time
 from bs4 import BeautifulSoup
-from googleapiclient.discovery import build
+from google import google
 import requests
 
 # delay in seconds before checking for new events
 SOCKET_DELAY = 1
 # slackbot environment variables
-my_api_key = os.environ.get('my_api_key')
-my_cse_id = os.environ.get('my_cse_id')
-SLACK_NAME = os.environ.get('SLACK_NAME')
-SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
-SLACK_ID = os.environ.get('SLACK_ID')
-slack_client = slackclient.SlackClient(SLACK_TOKEN)
 
 
-def is_for_me(event):
-    return event.get('user') == SLACK_ID
+VALET_SLACK_TOKEN='xoxb-227769392726-H7SOPPyTRn7K9dqNMRNhHAzq'
+VALET_SLACK_NAME="slack-test-bot"
+VALET_SLACK_ID='U6PNMBJMC'
+valet_slack_client = slackclient.SlackClient(VALET_SLACK_TOKEN)
 
 
-
+def handle_message(message, user, channel):
+    search_string = message + ' python -url"stack overflow"'
+    search_string.replace('<@U6PNMBJMC>','')
+    url = search_google(search_string)
+    print "got url %s" % url
+    code = scrape(url)
+    if code !=None:
+        for x in code:
+            reply = "```%s```" % (x.text)
+            post_message(message=reply, channel=channel)
+    else:
+        post_message(message="ERROR",channel=channel)
 
 def post_message(message, channel):
-    slack_client.api_call('chat.postMessage', channel=channel,
+    valet_slack_client.api_call('chat.postMessage', channel=channel,
                           text=message, as_user=True)
 
 def run():
-    if slack_client.rtm_connect():
+    if valet_slack_client.rtm_connect():
         print('[.] Valet de Machin is ON...')
         while True:
-            event_list = slack_client.rtm_read()
+            event_list = valet_slack_client.rtm_read()
             if len(event_list) > 0:
                 for event in event_list:
-                    print(event)
-                    if is_for_me(event):
-                        results = google_search(event.get('text'), my_api_key, my_cse_id, num=10)
-                        handle_message(search_term=results[0].get('link'),channel=event.get('channel'))
+                    if event.get('type') == 'message':
+                        reply = event.get('text')
+                        if reply == None:
+                            reply="none"
+                        print reply
+                        print event.get('channel')
+                        if (("<@U6PNMBJMC>" in reply) or (event.get('channel') == "D6NPML7S7")) and (event.get('user') != VALET_SLACK_ID)  :
+                            handle_message(message=event.get('text'), user=event.get('user'), channel=event.get('channel'))
             time.sleep(SOCKET_DELAY)
     else:
         print('[!] Connection to Slack failed.')
 
 
+# def search_google(search_string):
+#     search_result = google.search('%s in python -url"stack oveflow"' %(search_string),1)
+#     url = search_result[0].link
+#     return url
 
+def search_google(search):
+	url_list = []
+	search_result = requests.get('https://www.google.com/search?q=' + search)
+	soup = BeautifulSoup(search_result.text,"html.parser")
+	for anchor in soup.findAll('a', href=True):
+		x = 0
+		anchor = str(anchor)
+		while x<14:
+			if anchor[x] == '<a href="/url?'[x]:
+				r = True
+			else:
+				r = False
+			x = x+1
+		if r==True:
+			search_url = anchor
+			break
+	link_soup = BeautifulSoup(search_url,'html.parser')
+	tag = link_soup.a
+	tag.name = 'a'
+	url = 'https://'+'www.google.com'+tag['href']
+	return url
 
 def scrape(url):
+    print "scraping"
     url_obj_data  = requests.get(url)
     url_text_data = url_obj_data.text
     soup = BeautifulSoup(url_text_data,'html.parser')
     answer_element = soup.find("div", { "class" : "accepted-answer" })
+    if answer_element == None:
+        return None
     # print answer_element.prettify()
     pre = answer_element.findAll("pre")
     return pre
-
-
-def google_search(search_term, api_key, cse_id, **kwargs):
-    service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return res['items']
-
-
-def handle_message(url,channel):
-    code = scrape(url)
-    for x in code:
-        reply = "```%s```" % (x.text)
-        post_message(message=reply, channel=channel)
-
 
 if __name__=='__main__':
     run()
